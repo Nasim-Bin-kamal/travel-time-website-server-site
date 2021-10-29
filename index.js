3802c8e9d99d104f85ef1d33f72879daa62daafa
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2021-10-30 02:04:45 +0600

    get and post completed

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o8my7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const database = client.db('travelTimeDb');
+        const packageCollection = database.collection('packages');
+
+        //GET API
+        app.get('/packages', async (req, res) => {
+            const cursor = packageCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        });
+
+        //POST API 
+        app.post('/packages', async (req, res) => {
+            const newPackage = req.body;
+            console.log(newPackage);
+            const result = await packageCollection.insertOne(newPackage);
+            console.log(result);
+            res.send('found');
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello!! From travel time server app');
+});
+
+app.listen(port, () => {
+    console.log('listening from port :', port);
+});
+
+
